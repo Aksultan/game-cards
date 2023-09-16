18e522155fe6405b73316e0d6fdabcce3654ee24
Aksultan <aksultan.zhumadil@gmail.com>
2023-09-16 14:49:01 +0600

    feat: GameBoard layout update

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -4,8 +4,30 @@ import PlayingCard from '@/components/PlayingCard.vue';
 
 <template>
     <div class="game-board">
-        <playing-card card-value="Q" suit="heart"/>
-        <playing-card card-value="6" suit="spade"/>    
-        <playing-card card-value="K" suit="club"/>
+        <div class="empty-space"/>
+        <div class="cards-wrapper">
+            <playing-card card-value="Q" suit="heart"/>
+            <playing-card card-value="6" suit="spade"/>    
+            <playing-card card-value="K" suit="club"/>
+        </div>
     </div>
 </template>
+
+<style>
+.game-board {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+}
+
+.cards-wrapper {
+    display: flex;
+    gap: 20px;   
+}
+
+.empty-space {
+    height: 20rem;
+    width: 12rem;
+}
+</style>
